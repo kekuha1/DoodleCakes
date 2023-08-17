dae857c241bf329d524c159c1e44483c6d233d40
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-08-16 22:13:09 -0400

    yes sensei

---
+++ backend/models/productModel.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  user: {},
+
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+
+  brand: {
+    type: String,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  reviews: [reviewSchema],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
