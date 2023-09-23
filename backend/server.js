796b3dfb9505a9497486efa12d335ff463e4d12d
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-09-23 12:07:03 -0400

    resolved 500 error

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-const port = process.env.PORT || 5000;
+import productRoutes from "./routes/productRoutes.js";
+
+const port = process.env.PORT || 5050;
 
 connectDB();
 
@@ -13,13 +14,8 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
