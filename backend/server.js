45cee6bcaa7c93a2888afb71927a28908b5891dc
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-10-07 17:37:02 -0400

    added controller functions to backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
-
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5050;
 
 connectDB();
@@ -15,10 +15,11 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
-app.use
+app.use;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
