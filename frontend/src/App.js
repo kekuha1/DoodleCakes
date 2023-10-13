d3d18f86e2a87793e0d204154ece2c415d36f7e7
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-10-12 23:16:07 -0400

    initial commit and backup review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 }
